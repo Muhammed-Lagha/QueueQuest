28ff28b0403cafb9e7c4cd486278b66a4f78184d
Muhammed <lagha.muhammed@yahoo.com>
2025-01-18 16:15:15 +0200

    Resolve MassTransit consumer registration issue for OrderPlacedConsumer

--- ShippingService/Program.cs
+++ ShippingService/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddMassTransit(x =>
     });
 });
 
+builder.Services.AddScoped<ShippingService.Consumers.OrderPlacedConsumer>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
